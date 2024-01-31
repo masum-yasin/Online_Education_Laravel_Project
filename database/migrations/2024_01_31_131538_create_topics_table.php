b91011a3f84c08ed84739c1ccdbadefca1304333
masum-yasin <masum55549@gmail.com>
2024-01-31 19:22:09 +0600

    update

---
+++ database/migrations/2024_01_31_131538_create_topics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->string('topic_name');
+            $table->tinyInteger('course_categories_id');
+            $table->tinyInteger('lessons_id');
+            $table->text('description');
+            $table->boolean('status')->default(0);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('topics');
+    }
+};
